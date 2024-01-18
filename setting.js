a29ca8fcf5a160e1679a01d7b58f11159bc8ed86
yamadamomona <122075447+yamadamomona@users.noreply.github.com>
2024-01-18 16:11:05 +0900

    教員ページの修正

---
+++ setting.js
@@ -0,0 +1,36 @@
+window.fileToFileTransferVariable = {
+  "date": "",
+  "isLoadSettingSuccess": true,
+  "title": "福岡大学工学部",
+  "headline": "福岡大学工学部",
+  "headlineBlockId": null,
+  "isFullSize": true,
+  "isTopbar": false,
+  "url": "../",
+  "fontFamily": {
+    "Hannari": false,
+    "Kokoro": false,
+    "Nico Moji": false,
+    "Nikukyu": false,
+    "M PLUS 1p": false,
+    "M PLUS Rounded 1c": false,
+    "Sawarabi Mincho": false,
+    "Sawarabi Gothic": false,
+    "Noto Sans JP": true
+  },
+  "toolList": [],
+  "plugins": [],
+  "officeName": "福岡大学工学部",
+  "phoneNumber": "092-871-6631",
+  "extensionNumber": "",
+  "website": "http://www.tec.fukuoka-u.ac.jp",
+  "mapUrl": "https://maps.app.goo.gl/dMCdozde4hixtFyH6",
+  "address": "〒814-0180　福岡市城南区七隈八丁目19番１号",
+  "childPages": {
+    "blog_system": {
+      "headline": "お知らせ",
+      "overview": " TOP  学部長あいさつ 学科紹介 機械工学科 電気工学科 電子情報工学科 化学システム工学科 社",
+      "date": ""
+    }
+  }
+};
