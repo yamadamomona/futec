4907b834435341c2917cb7749eb390ccb27f2768
yamadamomona <m.yamada.qs@adm.fukuoka-u.ac.jp>
2024-08-09 12:48:30 +0900

    お知らせオーキャン

---
+++ blog_system/20240808_1526/setting.js
@@ -0,0 +1,63 @@
+window.fileToFileTransferVariable = {
+  "date": "2024-08-09",
+  "isLoadSettingSuccess": true,
+  "title": "福岡大学工学部",
+  "headline": "オープンキャンパス2024を開催しました(8/4)、工学部説明会の動画をアップしました",
+  "headlineBlockId": "block_ff2d5224-7846-45ff-b09b-4c16ea9d4438",
+  "thumbnailUrl": null,
+  "isFullSize": true,
+  "isTopbar": true,
+  "fontFamily": {
+    "Hannari": false,
+    "Kokoro": false,
+    "Nico Moji": false,
+    "Nikukyu": false,
+    "M PLUS 1p": false,
+    "M PLUS Rounded 1c": false,
+    "Sawarabi Mincho": false,
+    "Sawarabi Gothic": false,
+    "Noto Sans JP": true
+  },
+  "toolList": [
+    {
+      "blockType": "IFrame",
+      "children": []
+    },
+    {
+      "blockType": "TrashCan",
+      "children": []
+    },
+    {
+      "blockType": "H2",
+      "children": []
+    },
+    {
+      "blockType": "RedH2",
+      "children": []
+    },
+    {
+      "blockType": "Date",
+      "children": []
+    },
+    {
+      "blockType": "PageSetting",
+      "children": []
+    },
+    {
+      "blockType": "P",
+      "children": []
+    },
+    {
+      "blockType": "Padding",
+      "children": []
+    }
+  ],
+  "plugins": [],
+  "officeName": "福岡大学工学部",
+  "phoneNumber": "092-871-6631",
+  "extensionNumber": "",
+  "website": "http://www.tec.fukuoka-u.ac.jp",
+  "mapUrl": "https://maps.app.goo.gl/dMCdozde4hixtFyH6",
+  "address": "〒814-0180　福岡市城南区七隈八丁目19番１号",
+  "childPages": {}
+};
